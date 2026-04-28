f715f1030caa4fc3746b0b98410853efa9d6bc15
Giovani <giovani.aquino1612@gmail.com>
2026-04-28 18:56:54 -0300

    Implementação de findByEmail no UserRepository

--- src/main/java/com/aprender/security/repositories/UserRepository.java
+++ src/main/java/com/aprender/security/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package com.aprender.security.repositories;
 import com.aprender.security.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    Optional<UserModel> findByEmail(String email);
+
 }
