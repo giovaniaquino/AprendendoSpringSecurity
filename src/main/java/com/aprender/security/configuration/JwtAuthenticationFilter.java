95ab480378c5d3e5bd5cec5d3e2aaf3682de8ea3
Giovani <giovani.aquino1612@gmail.com>
2026-04-28 19:19:14 -0300

    Add comentari

--- src/main/java/com/aprender/security/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/aprender/security/configuration/JwtAuthenticationFilter.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
+    //private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -31,6 +31,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         // 7 is the position after "Bearer "
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
+        //userEmail = jwtService.extractUsername(jwt);
     }
 }
